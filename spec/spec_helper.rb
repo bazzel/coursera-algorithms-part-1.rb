c71f52d0fd37fba7f208febed41af19b7bf91f6b
Patrick Baselier <patrick@kabisa.nl>
2013-08-24 22:41:15 +0200

    Exclude specs tagged with performance

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 RSpec.configure do |c|
+  c.filter_run_excluding performance: true
   c.filter_run focused: true
   c.alias_example_to :fit, focused: true
   c.run_all_when_everything_filtered = true
