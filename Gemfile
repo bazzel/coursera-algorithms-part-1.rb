5cc0eb3738dd663e8b0f1487900ff9032fd168cc
Patrick Baselier <patrick@kabisa.nl>
2013-08-24 10:59:05 +0200

    Add yard

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'yard'
+
 group :test do
   gem 'guard'
   gem 'guard-rspec'
--- lib/quick_find_u_f.rb
+++ lib/quick_find_u_f.rb
@@ -1,14 +1,14 @@
 class QuickFindUF
 
-  # @param [FixNum] sites_count Number of connected sites (>0)
+  # @param [FixNum] sites_count Number of connected sites (> 0)
   def initialize(sites_count)
     @id = Array.new(sites_count) { |i| i }
   end
 
   # @return [Boolean] true if p and q are connected, false otherwise
   #
-  # @param [FixNum] p, q id of sites to be connected (0 <= p, q <
-  # sites_count)
+  # @param [FixNum] p id of site (0 <= p < sites_count)
+  # @param [FixNum] q id of site (0 <= q < sites_count)
   def connected?(p, q)
     @id[p] == @id[q]
   end
@@ -16,8 +16,8 @@ class QuickFindUF
   # To merge sites `p` and `q`, change all entries whose id
   # equals @id[p] to @id[q].
   #
-  # @param [FixNum] p, q id of sites to be connected (0 <= p, q <
-  # sites_count)
+  # @param [FixNum] p id of site to connect (0 <= p < sites_count)
+  # @param [FixNum] q id of site to connect to (0 <= q < sites_count)
   def union(p, q)
     p_id, q_id = @id[p], @id[q]
 
