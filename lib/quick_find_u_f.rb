07a8b5ebe1de0f88caa0a7a336b77278794a6d94
Patrick Baselier <patrick@kabisa.nl>
2013-08-24 10:49:03 +0200

    Document QuickFindUF

--- lib/quick_find_u_f.rb
+++ lib/quick_find_u_f.rb
@@ -1,13 +1,23 @@
 class QuickFindUF
 
+  # @param [FixNum] sites_count Number of connected sites (>0)
   def initialize(sites_count)
     @id = Array.new(sites_count) { |i| i }
   end
 
+  # @return [Boolean] true if p and q are connected, false otherwise
+  #
+  # @param [FixNum] p, q id of sites to be connected (0 <= p, q <
+  # sites_count)
   def connected?(p, q)
     @id[p] == @id[q]
   end
 
+  # To merge sites `p` and `q`, change all entries whose id
+  # equals @id[p] to @id[q].
+  #
+  # @param [FixNum] p, q id of sites to be connected (0 <= p, q <
+  # sites_count)
   def union(p, q)
     p_id, q_id = @id[p], @id[q]
 
