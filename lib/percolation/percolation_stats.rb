fe0d9d9102fe38d9d081a1319219ca1dc8ed75a8
Patrick Baselier <patrick@kabisa.nl>
2013-09-04 15:43:17 +0200

    Calculate stddev

--- lib/percolation/percolation_stats.rb
+++ lib/percolation/percolation_stats.rb
@@ -4,7 +4,7 @@ class PercolationStats
     def main(n, t)
       instance = new(n, t, QuickUnionUF)
 
-      "mean      = #{instance.mean}\nstddev      =\n95% confidence interval     ="
+      "mean      = #{instance.mean}\nstddev      =#{instance.standard_deviation}\n95% confidence interval     ="
     end
   end
 
@@ -16,7 +16,11 @@ class PercolationStats
   end
 
   def mean
-    open_sites_fractions.reduce(:+) / n
+    open_sites_fractions.mean
+  end
+
+  def standard_deviation
+    open_sites_fractions.standard_deviation
   end
 
   private
@@ -30,7 +34,26 @@ class PercolationStats
       open_sites_count += 1
     end
 
-    open_sites_count.to_f/n
+    open_sites_count.to_f/(n*n)
+  end
+end
+
+#  Add methods to Enumerable, which makes them available to Array
+module Enumerable
+  def mean
+    reduce(:+).to_f/size
   end
 
-end
+  #  variance of an array of numbers
+  def sample_variance
+    avg=mean
+    sum=inject(0){|acc,i|acc +(i-avg)**2}
+    sum.to_f/length
+  end
+
+  #  standard deviation of an array of numbers
+  def standard_deviation
+    Math.sqrt(sample_variance)
+  end
+
+end  #  module Enumerable
