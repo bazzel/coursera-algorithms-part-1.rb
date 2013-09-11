7299b5f5b0bb5ea8d56e67a2542c9d163d91a4fe
Patrick Baselier <patrick@kabisa.nl>
2013-09-11 08:54:45 +0200

    Add LinkedStackOfStrings

---
+++ lib/stacks_and_queues/linked_stack_of_strings.rb
@@ -0,0 +1,23 @@
+# Stack: linked-list representation
+# Maintain pointer to first node in a linked list; insert/remove from front.
+class LinkedStackOfStrings
+  Node = Struct.new(:item, :next)
+  attr_reader :first
+
+  def push(item)
+    @first = Node.new(item, first)
+  end
+
+  def pop
+    return if is_empty?
+
+    item = first.item
+    @first = first.next
+    item
+  end
+
+  def is_empty?
+    !first
+  end
+
+end
