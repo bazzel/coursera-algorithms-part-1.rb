c178051e1c55283b68072583825a4d6aa2960a2e
Patrick Baselier <patrick@kabisa.nl>
2013-09-11 13:43:26 +0200

    Delete it all

--- lib/stacks_and_queues/array_stack_of_strings.rb
+++ lib/stacks_and_queues/array_stack_of_strings.rb
@@ -1,13 +1,4 @@
 # Array implementation of a stack
 # No need to increase or decrease the elements' capacity
 # since this is already taken care of by Ruby.
-class ArrayStackOfStrings
-  extend Forwardable
-
-  attr_reader :elements
-  def_delegators :@elements, :push, :pop
-
-  def initialize
-    @elements = []
-  end
-end
+class ArrayStackOfStrings < Array; end
