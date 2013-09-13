909fd51d06edc454ded8fc4c6e96713c604374bd
Patrick Baselier <patrick@kabisa.nl>
2013-09-13 08:54:19 +0200

    Implement SelectionSort

--- lib/elementary_sort/selection_sort.rb
+++ lib/elementary_sort/selection_sort.rb
@@ -1,5 +1,22 @@
+# Algorithm. ↑ scans from left to right.
+# Invariants:
+#  ・Entries the left of ↑ (including ↑) fixed and in ascending order.
+#  ・No entry to right of ↑ is smaller than any entry to the left of ↑.
 module SelectionSort
-
   def sort
+    0.upto(self.size-1) do |i|
+      min = i
+      (i+1).upto(self.size-1) do |j|
+        if self[j] < self[min]
+          min = j
+        end
+
+      end
+      swap = self[i]
+      self[i] = self[min]
+      self[min] = swap
+    end
+
+    self
   end
 end
--- spec/lib/elementary_sort/selection_sort_spec.rb
+++ spec/lib/elementary_sort/selection_sort_spec.rb
@@ -4,7 +4,7 @@ require 'elementary_sort/selection_sort'
 describe SelectionSort do
   subject            { items.sort }
   let(:items)        { %w(foo bar baz qux quux corge grault garply waldo fred plugh xyzzy thud) }
-  let(:sorted_items) { %w(bar baz corge foo fred grault garply plugh quux qux thud waldo xyzzy) }
+  let(:sorted_items) { %w(bar baz corge foo fred garply grault plugh quux qux thud waldo xyzzy) }
 
   before { items.extend described_role }
 
